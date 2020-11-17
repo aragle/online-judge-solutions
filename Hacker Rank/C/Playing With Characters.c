57051e72b740659c5e807d25fcde34a3d505bc59
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 09:19:40 +0600

    Update Playing With Characters.c

--- Hacker Rank/C/Playing With Characters.c
+++ Hacker Rank/C/Playing With Characters.c
@@ -5,7 +5,13 @@
 
 int main() 
 {
-
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    char ch; char s[50]; char sen[100];
+    scanf("%c", &ch);
+    scanf("%s",&s);
+    scanf("\n");
+    scanf("%[^\n]%*c", &sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s", sen);
     return 0;
 }
