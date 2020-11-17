27c09d0d2fff0ae0c268e602ec1c195817c44a0c
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 10:45:42 +0600

    Create Basic Data Types.cpp

---
+++ Hacker Rank/CPP/Basic Data Types.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int i; long l; char ch; float f; double d;
+    scanf("%d %ld %c %f %lf",&i,&l,&ch,&f,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf",i,l,ch,f,d);
+    return 0;
+}
