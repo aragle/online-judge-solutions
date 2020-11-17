a9317de3a55946fb6b308d39319b14d751de42d2
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 09:20:40 +0600

    Update "Hello World!" in C.c

--- Hacker Rank/C/"Hello World!" in C.c
+++ Hacker Rank/C/"Hello World!" in C.c
@@ -7,8 +7,8 @@ int main()
 {
 	
     char s[100];
-    scanf("%[^\n]%*c", &s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    scanf("%[^\n]%*c", s);
+    printf("Hello, World!\n",s); 
+    printf("%s",s);   
     return 0;
 }
