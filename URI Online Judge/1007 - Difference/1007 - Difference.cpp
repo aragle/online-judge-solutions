1ba4f63c685d0083176b8aacbd525e38508f4c26
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 06:41:08 +0600

    Create  1007 - Difference.cpp

---
+++ URI Online Judge/1007 - Difference/1007 - Difference.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c,d,DIFERENCA=0;
+    cin >> a >> b >> c >> d;
+    DIFERENCA = (a * b - c * d);
+    cout << "DIFERENCA = " << DIFERENCA << endl;
+}
