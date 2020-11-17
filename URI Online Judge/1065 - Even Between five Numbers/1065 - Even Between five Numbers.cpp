323709caee9964904f2c0da15c6185d91ab40e3e
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 07:49:49 +0600

    Update 1065 - Even Between five Numbers.cpp

--- URI Online Judge/1065 - Even Between five Numbers/1065 - Even Between five Numbers.cpp
+++ URI Online Judge/1065 - Even Between five Numbers/1065 - Even Between five Numbers.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    int inputValue;
+    int count = 0;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        cin >> inputValue;
+        if(inputValue < 0)
+        {
+            inputValue = -inputValue;
+        }
+
+        if(inputValue % 2 == 0)
+        {
+            count++;
+        }
+    }
+    cout << count << " valores pares" << endl;
+}
