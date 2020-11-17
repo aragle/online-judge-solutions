9ab39d54522e2d747101c80255f2783dfebf8039
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 07:54:44 +0600

    Create 1067 - Odd Numbers.cpp

---
+++ URI Online Judge/1067 - Odd Numbers/1067 - Odd Numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int inputAnInteger;
+
+    cin >> inputAnInteger;
+
+    for (int i = 1; i <= inputAnInteger; i+=2){
+        cout << i << endl;
+    }
+}
