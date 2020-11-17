a8360da051ad1725c9dcb7a8e2db0c51f76fa23d
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-11-17 08:09:34 +0600

    Create 1096 - Sequence IJ 2.cpp

---
+++ URI Online Judge/1096 - Sequence IJ 2/1096 - Sequence IJ 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    for (int i = 1; i <= 9; i+=2){
+        cout << "I=" << i << " J=7\n";
+        cout << "I=" << i << " J=6\n";
+        cout << "I=" << i << " J=5\n";
+    }
+}
